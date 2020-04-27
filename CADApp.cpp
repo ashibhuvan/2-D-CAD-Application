82412b761a63e6ec1a0fa56b5f41721578700551
ashibhuvan <asishm.school@gmail.com>
2020-04-27 00:14:42 -0400

    testing visual studio built in git

--- CADApp.cpp
+++ CADApp.cpp
@@ -1,8 +1,90 @@
 // CADApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+
+#define OLC_PGE_APPLICATION
 #include <iostream>
 #include "olcPixelGameEngine.h"
+
+
+//static variables
+
+float sShape::worldScale = 1.0f;
+
+
+
+class CADApplication : public olc::PixelGameEngine {
+
+public:
+    CADApplication() {
+    
+    //what
+        sAppName = "CAD Application";
+    }
+
+private:
+    olc::vf2d vOffset = { 0.0f,0.0f };
+    olc::vf2d vStartPan = { 0.0f, 0.0f };
+
+
+    float fScale = 10.0f;
+    float fGrid = 1.0f;
+    
+    //convert coordinates from world space to screenspace
+
+    void WorldtoScreen(const olc::vf2d &v, int &nScreenX, int &nScreenY) 
+    {
+        nScreenX = (int)((v.x - vOffset.x) * fScale);
+        nScreenY = (int)((v.y - vOffset.y) * fScale);
+    }
+
+    // Convert coordinates from Screen Space --> World Space
+    void ScreenToWorld(int nScreenX, int nScreenY, olc::vf2d& v)
+    {
+        v.x = (float)(nScreenX) / fScale + vOffset.x;
+        v.y = (float)(nScreenY) / fScale + vOffset.y;
+    }
+
+
+
+
+public:
+
+    bool OnUserCreate() override 
+    {
+        //set the intial coordinates
+        //configure world space to be middle of screen
+        vOffset = { (float)(-ScreenWidth() / 2) / fScale , (float)(-ScreenHeight() / 2) / fScale };
+        //vOffset = { (float)(-ScreenWidth() / 2) / fScale, (float)(-ScreenHeight() / 2) / fScale };
+        return true;
+    }
+
+    bool OnUserUpdate(float fElapsedTime) override
+    {
+        
+        //get the mouse coordinates
+        olc::vf2d vMouse = {(float)GetMouseX(), (float)GetMouseY() };
+
+        //handle pan and zoom
+        if (GetMouse(2).bPressed)
+        {
+            vStartPan = vMouse;
+
+        }
+        if (GetMouse(2).bHeld) {
+            vOffset -= (vMouse - vStartPan) / fScale;
+            vStartPan = vMouse;
+        }
+
+        olc::vf2d vMouseBeforeZoom;
+
+    
+    }
+
+};
+
+
+
 
 
 
